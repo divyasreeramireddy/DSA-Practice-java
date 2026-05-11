505e67fa6aaf7df3d8c3b556aaf34892a0d5cd97
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:27:30 +0530

    Add palindrome check method in Solution class
    Implement palindrome check for integers, including negative numbers.

--- Basics/Palindrome.java
+++ Basics/Palindrome.java
@@ -1,1 +1,18 @@
-
+//checks if a number is a palindrome or not(works for negative numbers also by converting into absolute value)
+class Solution {
+    public boolean isPalindrome(int n) {
+        // code here
+        boolean ispalin=true;
+        n = Math.abs(n);
+        String s = Integer.toString(n);
+        String rev = "";
+        for(int i=s.length()-1;i>=0;i--){
+            rev = rev + s.charAt(i);
+        }
+        int digit = Integer.parseInt(rev);
+        if(n!=digit){
+            ispalin = false;
+        }
+    return ispalin;
+    }
+}
