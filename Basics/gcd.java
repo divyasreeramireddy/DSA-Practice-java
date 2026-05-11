530bae0644a6e4d931b6a55287d2700e243da7c5
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:12:34 +0530

    Add gcd function in Solution class
    Implement gcd function to find the greatest common divisor of two numbers.

--- Basics/gcd.java
+++ Basics/gcd.java
@@ -1,1 +1,12 @@
-
+//find gcd of given two numbers.
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+         while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+       return a;
+    }
+}
