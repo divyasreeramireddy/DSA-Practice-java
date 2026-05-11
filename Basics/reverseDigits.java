16e43f2a9e6e49d205b58d58337a1adf35aa19b3
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:19:03 +0530

    Add reverseDigits method to reverse an integer
    Implement reverseDigits method to reverse an integer.

--- Basics/reverseDigits.java
+++ Basics/reverseDigits.java
@@ -1,1 +1,13 @@
-
+//print the reverse of a number by eliminatinng leading zeroes if any.
+class Solution {
+    public int reverseDigits(int n) {
+        // Code here
+        String rev="";
+        String s = Integer.toString(n);
+        for(int i=s.length()-1;i>=0;i--){
+           rev = rev + s.charAt(i);
+        }
+        int ans = Integer.parseInt(rev);
+        return ans;
+    }
+}
