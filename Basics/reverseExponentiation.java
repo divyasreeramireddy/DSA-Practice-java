3110a7cf4000ec97fa6439707b7f6ac9f0bc4eda
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:05:42 +0530

    Add reverseExponentiation method in Solution class
    Implement reverseExponentiation method to reverse digits and compute power.

--- Basics/reverseExponentiation.java
+++ Basics/reverseExponentiation.java
@@ -1,1 +1,16 @@
+//You are given an integer n. Your task is to reverse the digits, ensuring that the reversed number has no leading zeroes.
+class Solution {
+    public int reverseExponentiation(int n) {
+        // code here
+        String rev = "";
+        String s = Integer.toString(n);
+        for(int i=s.length()-1;i>=0;i--){
+            rev =  rev + s.charAt(i);
+        }
+        int digit = Integer.parseInt(rev);
+        int ans = (int)Math.pow(n,digit);
+    return ans;
+    }
+}
+
 
