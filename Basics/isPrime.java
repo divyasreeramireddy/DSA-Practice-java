f4437b9d2df0f7fd4cab14af40fe8224fece40ab
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:16:33 +0530

    Add isPrime function in Solution class
    Implement isPrime function to check for prime numbers.

--- Basics/isPrime.java
+++ Basics/isPrime.java
@@ -1,1 +1,19 @@
-
+//check if given number is prime or not
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        boolean isprime = true;
+        if(n==2){
+            isprime =true;
+        }
+        if(n==1){
+            isprime = false;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+            isprime = false;
+            }
+        }
+    return isprime;
+    }
+}
