b5f554fb571bc9e7ae3be97f34a40a74a123dea2
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:15:06 +0530

    Implement divisor finding method in FindDivisors.java
    Fix the implementation to find all divisors of a number.

--- Basics/FindDivisors.java
+++ Basics/FindDivisors.java
@@ -1,1 +1,13 @@
-
+//find all the divisors of a given number.
+import java.util.*;
+public class Solution {
+    public static List<Integer> printDivisors(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
