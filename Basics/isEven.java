03f6277ca29b0993ed204334ad5b747eab6107c6
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-11 18:08:31 +0530

    Add isEven method to determine even numbers
    Implement isEven method to check if a number is even.

--- Basics/isEven.java
+++ Basics/isEven.java
@@ -1,1 +1,12 @@
-
+//check if a number is even or odd and return boolean type
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if(n%2==0){
+           return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
