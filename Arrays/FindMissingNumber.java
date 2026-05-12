c9c7ef1d778ec49986128a6f176249edf3ee7cac
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-12 23:28:14 +0530

    Add method to find missing number in array
    Implement logic to find the missing number in a sorted array of consecutive integers.

--- Arrays/FindMissingNumber.java
+++ Arrays/FindMissingNumber.java
@@ -1,1 +1,17 @@
-
+//gives an array with consecutive integers from 1 to n find the missing element in array
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        Arrays.sort(arr);
+       int n = arr.length;
+       if(arr[0]!=1){
+           return 1;
+       }
+           for(int i =0;i<n-1;i++){
+               if(arr[i+1]!=arr[i]+1){
+                 return arr[i]+1;
+               }
+           }
+       return arr[n-1]+1;
+    }
+}
