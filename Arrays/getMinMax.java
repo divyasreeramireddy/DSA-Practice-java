f8eb653784a8a53329634194bec5cb6d2b4ec181
divyasreeramireddy <saidivya0806@gmail.com>
2026-05-14 13:12:36 +0530

    Add method to find minimum and maximum elements
    Implemented getMinMax method to find min and max in an array.

--- Arrays/getMinMax.java
+++ Arrays/getMinMax.java
@@ -1,1 +1,13 @@
+//Given an array arr[]. Your task is to find the minimum and maximum elements in the array.
+class Solution {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+     Arrays.sort(arr);
+    
+        ArrayList<Integer> list = new ArrayList<>();
 
+        list.addAll(Arrays.asList(arr[0], arr[arr.length - 1]));
+
+        return list;
+    }
+}
